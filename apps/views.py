48db4b7f62410e4f8ba22a1a734a95ebaba5766f
donyfajarr <donyfajarr@gmail.com>
2024-04-02 14:33:53 +0700

    register linked

--- apps/views.py
+++ apps/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render,redirect
-from django.template.response import render_to_response
 from redminelib import Redmine
 from redminelib.exceptions import AuthError
 import ssl
@@ -931,12 +930,12 @@ def newissue(request, redmine):
         return redirect('listproject')
     
 def handler404(request, exception, template_name='404.html'):
-    response = render_to_response(template_name)
+    response = render(template_name)
     response.status_code = 404
     return response
 
 def handler500(request,template_name='500.html'):
-    response = render_to_response(template_name)
+    response = render(template_name)
     response.status_code = 500
     return response
 
